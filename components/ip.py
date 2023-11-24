beebf58274d53084d25701829bac46c281f4edb7
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 13:28:35 +0530

    Feat: Add compareIPs function to compare two IPs
    Added a function compareIPs() to compare two IP addresses and return
    a boolean value indicating whether they are equal.
    The function also has functionality to read the last IP from a file.
    
    Signed-off-by: @surajgirioffl

--- components/ip.py
+++ components/ip.py
@@ -47,5 +47,62 @@ def getCurrentIP(sourceAPIEndpoint: str = "https://api.ipify.org/") -> str:
             return ""
 
 
+def compareIPs(
+    currentIP: str,
+    lastIP: str = None,
+    readLastIpFromFile: bool = True,
+    filePath: str = "appdata/last-ip.txt",
+    returnTrueIfFileNotFound: bool = True,
+) -> bool:
+    """
+    Description:
+        - Compare two IP addresses and return a boolean value indicating whether they are equal.
+
+    Args:
+        * currentIP (str):
+            - The IP address to compare.
+        * lastIP (str, optional):
+            - The last IP address to compare.
+            - If it will be given then none of next parameters will be used. It has max priority.
+            - Defaults to None.
+        * readLastIpFromFile (bool, optional):
+            - Whether to read the last IP address from a file.
+            - Defaults to True.
+        * filePath (str, optional):
+            - The path to the file containing the last IP address.
+            - Defaults to "appdata/last-ip.txt".
+        * returnTrueIfFileNotFound (bool, optional):
+            - Whether to return True if the file is not found.
+            - True is better in case file is not yet created.
+            - Defaults to True.
+
+    Returns:
+        * bool:
+            - True if the IP addresses are equal, False otherwise.
+    """
+    if readLastIpFromFile and lastIP is None:
+        try:
+            with open(filePath) as file:
+                lastIP: str = file.read()
+            lastIP: str = lastIP.strip()
+        except FileNotFoundError as e:
+            if returnTrueIfFileNotFound:
+                return True
+            print(f"File '{filePath}' not found. Error Code: 2103")
+            print("Exception:", e)
+            return False
+        except Exception as e:
+            print(
+                f"Something went wrong while reading last IP from the file. Error Code: 2104"
+            )
+            print("Exception:", e)
+            return False
+        else:
+            return True if currentIP == lastIP else False
+
+    return True if currentIP == lastIP else False
+
+
 if __name__ == "__main__":
     print(getCurrentIP())
+    print(compareIPs("127.0.0.1"))
