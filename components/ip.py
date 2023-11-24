7bb8af7c0465b8d51abd34db1f0ddc04b820e2d5
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 12:30:05 +0530

    Feat: Add Feat. To Fetch Current IP Address
    Add a function getCurrentIP() to fetch the current IP Address from
    the specified API.
    
    Signed-off-by: @surajgirioffl

--- components/ip.py
+++ components/ip.py
@@ -11,3 +11,41 @@
 
 __author__ = "Suraj Kumar Giri"
 __email__ = "surajgirioffl@gmail.com"
+
+import requests
+
+
+def getCurrentIP(sourceAPIEndpoint: str = "https://api.ipify.org/") -> str:
+    """
+    Description:
+        - Function to fetch the current IP from the specified API.
+
+    Args:
+        * sourceAPIEndpoint (str, optional):
+            - API endpoint to fetch the current IP.
+            - Defaults to "https://api.ipify.org/".
+
+    Returns:
+        * str:
+            - Return the current IP on success else an empty string.
+
+    Future:
+        - In future, we can add functionality to use API credentials, if required.
+        - Currently, as per project need, we don't need any credentials.
+    """
+    try:
+        response = requests.get(sourceAPIEndpoint)
+    except Exception as e:
+        print("Something went wrong while fetching current IP. Error Code: 2101")
+        print("Exception:", e)
+    else:
+        if response.status_code == 200:
+            return response.text.strip()
+        else:
+            print("Something went wrong while fetching current IP. Error Code: 2102")
+            print("Status Code:", response.status_code)
+            return ""
+
+
+if __name__ == "__main__":
+    print(getCurrentIP())
