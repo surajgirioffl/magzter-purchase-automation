d491565b123f72a2f734252b281d3c06a742e0df
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 12:24:27 +0530

    Docs: Update the module docstring
    Update the module docstring.
    
    Signed-off-by: @surajgirioffl

--- components/ip.py
+++ components/ip.py
@@ -1,12 +1,12 @@
 """
-    @file: components/api.py
+    @file: components/ip.py
     @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
     @init-date: 24th Nov 2023
     @completed-on: N/A
     @last-modified: 24th Nov 2023
     @error-series: 2100
     @description:
-        * Module to perform any operations related to API for the project including fetching the current IP, IP comparison and more..
+        * Module to perform any operations related to IP for the project including fetching the current IP, IP comparison and more..
 """
 
 __author__ = "Suraj Kumar Giri"
