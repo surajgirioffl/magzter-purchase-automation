6367f8ee3771c81952dd816abcbe61f32ff4e917
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 10:33:17 +0530

    Add: Add function createAppRequiredDirectories()
    Added the function 'createAppRequiredDirectories()' to create all
    required directories of the application.
    
    Signed-off-by: @surajgirioffl

--- utilites/tools.py
+++ utilites/tools.py
@@ -11,3 +11,34 @@
 
 __author__ = "Suraj Kumar Giri"
 __email__ = "surajgirioffl@gmail.com"
+
+import os
+
+
+def createAppRequiredDirectories(
+    directoriesWithPath: list | tuple | set = {"appdata"},
+) -> None:
+    """
+    Description:
+        - Function to create all required directories of the application.
+
+    Args:
+        * directoriesWithPath (list | tuple | set, optional):
+                - List of directories to create.
+                    - You can specify multiple directories along with paths.
+                    - If no path provided then it will taken as current working directory (root directory of the project/application)
+                    - You can also provided hierarchical directories path, all directories of the hierarchy will be created.
+                - Defaults to {"appdata"}.
+    Returns:
+        * None
+    """
+    try:
+        for directory in directoriesWithPath:
+            os.makedirs(directory, exist_ok=True)
+    except Exception as e:
+        print("Unable to create directories problems/language1,2.. Error Code: 3101")
+        print("Exception:", e)
+
+
+if __name__ == "__main__":
+    createAppRequiredDirectories()
