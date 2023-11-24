6d8f250dace66c9ddfd6a4be6956553de88b29ff
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 11:44:45 +0530

    Add: Add `api.py` module in the `components` package
    This module handles operations related to the API for the project,
    including fetching the current IP, IP comparison and more..
    
    Signed-off-by: @surajgirioffl

---
+++ components/api.py
@@ -0,0 +1,13 @@
+"""
+    @file: components/api.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 24th Nov 2023
+    @error-series: 2100
+    @description:
+        * Module to perform any operations related to API for the project including fetching the current IP, IP comparison and more..
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
