d8d7e2e5bceb043cf01ee9ed4c5be6f97399a953
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 10:10:06 +0530

    Add: Introduce tools Module for Additional App Operations
    This commit includes the introduction of the 'tools' module within
    the 'utilites' package. The 'tools' module is designed to perform
    additional operations required by the application. This modular
    approach enhances the organization and flexibility of the codebase.
    
    Signed-off-by: @surajgirioffl

---
+++ utilites/tools.py
@@ -0,0 +1,13 @@
+"""
+    @file: app_scripts/tools.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 24th Nov 2023
+    @error-series: 3100
+    @description:
+        * Module to perform any extra operations required for the project.
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
