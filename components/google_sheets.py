6af723140187edaa87de15eb233c050ea9b4f1fe
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-24 16:47:10 +0530

    Add: Add `google_sheet` module
    Add a module named google_sheet to handle operations related to
    Google Sheets for the project.
    
    Signed-off-by: @surajgirioffl

---
+++ components/google_sheets.py
@@ -0,0 +1,13 @@
+"""
+    @file: components/google_sheets.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 24th Nov 2023
+    @error-series: 2200
+    @description:
+        * Module to perform any operation related to the Google Sheets required for the project.
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
