161bcf26632b88b999202c8e95722c6130e74428
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-25 17:20:23 +0530

    Add: Add module `magzter.py` in the `components` package
    Adde a new module named magzter.py in the components package to handle
    operations related to Magzter in the project.
    
    Signed-off-by: @surajgirioffl

---
+++ components/magzter.py
@@ -0,0 +1,13 @@
+"""
+    @file: components/magzter.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 25th Nov 2023
+    @error-series: 2400
+    @description:
+        * Module to perform any operations related to magzter for the project.
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
