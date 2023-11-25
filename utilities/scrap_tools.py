51e068f002b2ab46b79f42c339acf21fcca9d449
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-25 17:29:33 +0530

    Add: Add module `scrap_tools.py` in `utilities` package
    Add a new module named scrap_tools.py in the utilities package to provide
    specific tools required while performing scraping in the project.
    
    Signed-off-by: @surajgirioffl

---
+++ utilities/scrap_tools.py
@@ -0,0 +1,12 @@
+"""
+    @file: utilities/scrap_tools.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 25th Nov 2023
+    @completed-on: N/A
+    @last-modified: 25th Nov 2023
+    @error-series: 3200
+    @description:
+        * Module to provide specific tools requires while performing scraping.
+"""
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
