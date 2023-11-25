1241c7952006f71f4d0df5182addfd207f2da0c4
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-25 17:18:49 +0530

    Add: Add module `microsoft.py` in components package
    Adde a new module named microsoft.py in the components package to handle
    operations related to Microsoft in the project.
    
    Signed-off-by: @surajgirioffl

---
+++ components/microsoft.py
@@ -0,0 +1,16 @@
+"""
+    @file: components/microsoft.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 25th Nov 2023
+    @error-series: 2300
+    @description:
+        * Module to perform any operations related to Microsoft for the project.
+"""
+
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
