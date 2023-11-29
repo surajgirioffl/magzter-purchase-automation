2abb1024ae11a6ec9fe27caab0822a0fd0350d10
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-29 14:31:20 +0530

    Add: Add `Spreadsheet` class along with constructor and destructor
    This class is added to handle database-related operations on spreadsheets,
    including establishing a connection with the database and creating the required table.
    
    Also, add constructor to establish a connection and create tables if
    required and destructor to commit the connection and destroy the object.
    
    Signed-off-by: @surajgirioffl

--- db_scripts/spreadsheet_db.py
+++ db_scripts/spreadsheet_db.py
@@ -10,3 +10,59 @@
 """
 __author__ = "Suraj Kumar Giri"
 __email__ = "surajgirioffl@gmail.com"
+
+import sqlite3 as sqlite
+from db_scripts import schema
+
+
+class Spreadsheet:
+    """
+    @description:
+        - Class to handle database related operations on spreadsheet.
+    """
+
+    def __init__(self, databaseName: str, tableName: str, path: str = "appdata/databases") -> None:
+        """
+        Description:
+            - Constructor to instantiate Spreadsheet object.
+
+        Args:
+            * databaseName(str):
+                - Name of the database to connect.
+                - It will same as the name of the spreadsheet.
+            * tableName(str):
+                - Table name to be created if not exits.
+                - It will same as the name of the sheet.
+            * path(str):
+                - Path to store/fetch the database.
+                - Defaults to 'appdata/databases'
+        Returns:
+            * None
+        """
+        try:
+            self.conn = sqlite.connect(f"{path}/{databaseName}.db")
+            self.cursor = self.conn.cursor()
+            self.connectionStatus = True
+            self.tableName = tableName
+            self.databaseName = databaseName
+        except Exception as e:
+            print("Connection failed. Error code 4201")
+            print("Exception:", e)
+            # To check if connection is established or not.
+            self.connectionStatus = False
+        else:
+            try:
+                self.cursor.execute(schema.Spreadsheet.getIPTableSchema(tableName))
+                self.conn.commit()
+            except Exception as e:
+                print(f"Unable to create table {tableName}. Error Code: 4202")
+                print("Exception:", e)
+
+    def __del__(self) -> None:
+        """
+        @description:
+            - Destructor to destroy the instance.
+        """
+        if self.connectionStatus:
+            self.conn.commit()
+            self.conn.close()
