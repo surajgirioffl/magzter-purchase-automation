d184b0c756bdb95548f2025dfcb3f6645e7e8c10
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-29 14:29:07 +0530

    Add: Add `spreadsheet_db` module in `db_scripts` package
    This module is added to handle database-related operations on spreadsheets.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts/spreadsheet_db.py
@@ -0,0 +1,12 @@
+"""
+    @file: db_scripts/spreadsheet_db.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 24th Nov 2023
+    @completed-on: N/A
+    @last-modified: 29th Nov 2023
+    @error-series: 4200
+    @description:
+        - Module to handle database related operations on spreadsheet.
+"""
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
