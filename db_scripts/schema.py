2fb791cbabd133d5ebd874c60c92e977d6c498d0
surajgirioffl <surajkumargiri2002@gmail.com>
2023-11-29 14:03:56 +0530

    Add: Add schema module for database tables
    Introduced the schema module to define the tables and their structure
    for the databases.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts/schema.py
@@ -0,0 +1,12 @@
+"""
+    @file: db_scripts/schema.py
+    @author: Suraj Kumar Giri (https://github.com/surajgirioffl)
+    @init-date: 29th Nov 2023
+    @completed-on: N/A
+    @last-modified: 29th Nov 2023
+    @error-series: 4100
+    @description:
+            - Module containing schema for tables of databases(classname).
+"""
+__author__ = "Suraj Kumar Giri"
+__email__ = "surajgirioffl@gmail.com"
